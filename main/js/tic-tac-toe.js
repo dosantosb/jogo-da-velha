d68f71337a1274ac06755931457d82238e050446
Marcos <marcos.santosbraga6@gmail.com>
2024-07-02 21:33:29 -0300

    Dando mais semântica ao código. (colocando a tag main novamente)

--- main/js/tic-tac-toe.js
+++ main/js/tic-tac-toe.js
@@ -1,9 +1,12 @@
+
+function startPlayer() {
+ 
+}
+
 document.addEventListener("DomContentLoaded", function() {
     const newGameButton = document.getElementById("new-game-button");
 
     newGameButton.addEventListener("click", function() {
-
-        
 
         // Inicio do jogo
         // Ao ser clicado, "New game" deve sumir e ficar aparecendo "x turn" e "o turn", conforme a vez dos jogadores.
